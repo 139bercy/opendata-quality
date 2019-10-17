f5d055ec17bd8ccd3de965bb2de9314048b1d2ee
Stéphane Trainel <stephane.trainel@gmail.com>
2019-10-17 23:27:22 +0200

    update first

--- src/first.R
+++ src/first.R
@@ -1,10 +1,17 @@
 
-library(stringr)
+require(stringr)
+require(rjson)
 
-file <- "https://data.economie.gouv.fr/explore/dataset/marques-francaises/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true"
+warning <- 0.1 # 1 pour 1000
+dataset_id <- "marques-francaises"
+
+api_data <- paste0("https://data.economie.gouv.fr/explore/dataset/", dataset_id, "/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true")
+api_meta <- paste0("https://data.economie.gouv.fr/api/v2/catalog/datasets/", dataset_id)
+
+meta <- fromJSON(file = api_meta)
 
 # Chargement du fichier et import en dataframe
-data <- read.csv2(file)
+data <- read.csv2(api_data)
 
 # Mdification des noms des variables
 myvar <- c("DATE_DEPOT", "TYPE", "CLASSES", "LIBELLE_MARQUE", "ID_MARQUE", "DATE_EXPIRATION")
@@ -35,3 +42,6 @@ nb1 <- length(data$DATE_DEPOT_CTRL[data$DATE_DEPOT_CTRL==FALSE])
 nb2 <- length(data$DATE_EXPIRATION_CTRL[data$DATE_EXPIRATION_CTRL==FALSE])
 nb3 <- length(data$CLASSES_CTRL[data$CLASSES_CTRL==FALSE])
 nb <- sum(nb1, nb2, nb3)
+
+# Enregistrement des résultats
+new_record <- c(dataset_id, meta$dataset$metas$default$title, nb > 0, nb > nrow * warning / 100, nrow, nb, c(nb1, nb2, nb3))
